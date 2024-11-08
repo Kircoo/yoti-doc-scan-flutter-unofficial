5244c6ef5f5c914d807fd1119067726d735dd2b0
Kircho Sharkovski <kircho@checkboard.com>
2024-11-08 14:36:47 +0100

    ios change

--- ios/Classes/YotiFlutterPlugin.swift
+++ ios/Classes/YotiFlutterPlugin.swift
@@ -1,77 +1,124 @@
 import Flutter
 import UIKit
+
 import YotiSDKCore
-import YotiSDKFaceTec
 import YotiSDKIdentityDocument
 import YotiSDKSupplementaryDocument
+import YotiSDKFaceTec
 import YotiSDKFaceCapture
 
-public class YotiFlutterPlugin: NSObject, FlutterPlugin, YotiSDKDataSource, YotiSDKDelegate  {
-
-  public func navigationController(_ navigationController: YotiSDKCore.YotiSDKNavigationController, didFinishWithResult result: YotiSDKResult) {
-      UIApplication.shared.keyWindow?.rootViewController?.dismiss(animated: true)
-      
-        switch result {
-          case .success:
-              self.flutterResult!(Int(0))
-              break
-          case .failure(let error):
-              self.flutterResult!(Int(error.errorCode));
-              break;
-          }
-  }
-  
-  public func sessionID(for navigationController: YotiSDKNavigationController) -> String {
-      self.sessionIdString
-  }
-
-  public func sessionToken(for navigationController: YotiSDKNavigationController) -> String {
-      self.sessionTokenString
-  }
-  
-  public func isReactNativeClient(for navigationController: YotiSDKNavigationController) -> Bool {
-      true
-  }
-
-  public func supportedModuleTypes(for navigationController: YotiSDKNavigationController) -> [YotiSDKModule.Type] {
-      [YotiSDKIdentityDocumentModule.self, YotiSDKFaceTecModule.self]
-  }
-
-  var sessionIdString = "";
-  var sessionTokenString = "";
-  var flutterResult: FlutterResult?
-
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "yoti_flutter", binaryMessenger: registrar.messenger())
-    let instance = YotiFlutterPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "startYoti":
-      
-      let arguments = call.arguments as? [String: Any]
-      let sessionId: String = arguments?["sessionId"] as! String;
-      let sessionToken: String = arguments?["sessionToken"] as! String;
-
-      self.startYoti(sessionId: sessionId ?? "", sessionToken: sessionToken ?? "", result: result)
-    default:
-      result(FlutterMethodNotImplemented)
+@main
+@objc class AppDelegate: FlutterAppDelegate, YotiSDKDataSource, YotiSDKDelegate {
+    var yotiChannel: FlutterMethodChannel?
+    var yotiNavigationController: YotiSDKNavigationController?
+    var sessionId: String?
+    var sessionToken: String?
+    
+    
+    
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        
+        let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
+        yotiChannel = FlutterMethodChannel(
+            name: "mobile.yoti.com/docscan",
+            binaryMessenger: controller.binaryMessenger
+        )
+        yotiChannel?.setMethodCallHandler({
+            [weak self] (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+            guard call.method == "startDocScan" else {
+                result(FlutterMethodNotImplemented)
+                return
+            }
+            
+            guard let arguments = call.arguments as? [String: Any],
+                  let sessionId = arguments["sessionId"] as? String,
+                  let sessionToken = arguments["sessionToken"] as? String else {
+                result(FlutterError(
+                    code: "INVALID_ARGUMENTS",
+                    message: "Invalid arguments for startDocScan.",
+                    details: nil
+                ))
+                return
+            }
+            
+            self?.sessionId = sessionId
+            self?.sessionToken = sessionToken
+            self?.startDocScan(result: result)
+        })
+        
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
-  }
-
-  private func startYoti(sessionId: String, sessionToken: String, result: @escaping FlutterResult) {
-      self.sessionIdString = sessionId;
-      self.sessionTokenString = sessionToken;
-      self.flutterResult = result;
-      let navigationController = YotiSDKNavigationController()
-      navigationController.sdkDataSource = self
-      navigationController.sdkDelegate = self
-      
-      UIApplication.shared.keyWindow?.rootViewController?.present(
-          navigationController, animated: true, completion: nil)
-
-  }
-
+    
+    private func startDocScan(result: @escaping FlutterResult) {
+        guard let rootViewController = window?.rootViewController else {
+            result(FlutterError(
+                code: "UNAVAILABLE",
+                message: "Root view controller not available.",
+                details: nil
+            ))
+            return
+        }
+        
+        if yotiNavigationController?.presentingViewController != nil {
+            print("Yoti Doc Scan is already presented!")
+            return
+        }
+        
+        print("Session ID: \(sessionId ?? "")")
+        print("Session Token: \(sessionToken ?? "")")
+        
+        yotiNavigationController = YotiSDKNavigationController()
+        yotiNavigationController?.sdkDataSource = self
+        yotiNavigationController?.sdkDelegate = self
+        rootViewController.present(yotiNavigationController!, animated: true, completion: nil)
+    }
+    
+    // MARK: - YotiSDKDataSource
+    
+    func supportedModuleTypes(for navigationController: YotiSDKNavigationController) -> [YotiSDKModule.Type] {
+        [
+            YotiSDKIdentityDocumentModule.self,
+            YotiSDKSupplementaryDocumentModule.self,
+            YotiSDKFaceTecModule.self,
+            YotiSDKFaceCaptureModule.self
+        ]
+    }
+    
+    func sessionID(for navigationController: YotiSDKNavigationController) -> String {
+        sessionId ?? ""
+    }
+    
+    func sessionToken(for navigationController: YotiSDKNavigationController) -> String {
+        sessionToken ?? ""
+    }
+    
+    // MARK: - YotiSDKDelegate
+    
+    func navigationController(_ navigationController: YotiSDKNavigationController, didFinishWithResult result: YotiSDKResult) {
+        guard let rootViewController = window?.rootViewController else {
+            return
+        }
+        rootViewController.dismiss(animated: true)
+        switch result {
+        case .success:
+            let docScanResult: [String: Any] = [
+                "sessionStatusCode": 0,  // Set the success status code
+                "sessionStatusDescription": "Success"
+            ]
+            self.yotiChannel?.invokeMethod("onYotiSdkResult", arguments: docScanResult)
+        case .failure(let error):
+            let errorCode = error.errorCode
+            let errorDescription = error.localizedDescription
+            
+            let docScanResult: [String: Any] = [
+                "sessionStatusCode": errorCode,
+                "sessionStatusDescription": "Error: \(errorDescription)"
+            ]
+            self.yotiChannel?.invokeMethod("onYotiSdkResult", arguments: docScanResult)
+        }
+    }
 }
